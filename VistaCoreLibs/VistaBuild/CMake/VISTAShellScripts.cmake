059dff580495a1c766586b25b8fc554b64f14242
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-02-15 14:27:51 +0000

    UPD: cleaned cmake files
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/Vista/trunk@20313 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- VistaCoreLibs/VistaBuild/CMake/VISTAShellScripts.cmake
+++
@@ -1,7 +0,0 @@
-function(CREATE_SHELLSCRIPT FILE_OUT APPCODE)
-# TODO Support for Windows
-    configure_file(
-        ${VISTA_DIR}/vista_set_paths.sh_proto
-        ${FILE_OUT}
-    )
-endfunction(CREATE_SHELLSCRIPT)
