5917c4e9575c23bff8adda644f0b4d9bb98023f7
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-03-03 15:25:19 +0000

    ADD: missed two files on last checkin
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/Vista/trunk@20460 33c38b33-09d2-4561-8d4d-1bfa2f58f885

---
+++ VistaCoreLibs/VistaKernel/_SourceFiles.cmake
@@ -0,0 +1,59 @@
+set( RelativeDir "." )
+set( LocalSourceGroup "Source Files" )
+set( SubDirs DisplayManager EventManager GraphicsManager InteractionManager OpenSG PickManager Stuff WindowingToolkit )
+
+set( DirFiles
+	VistaClientDataTunnel.cpp
+	VistaClientDataTunnel.h
+	VistaClusterAux.cpp
+	VistaClusterAux.h
+	VistaClusterClient.cpp
+	VistaClusterClient.h
+	VistaClusterMaster.cpp
+	VistaClusterMaster.h
+	VistaClusterServer.cpp
+	VistaClusterServer.h
+	VistaClusterSlave.cpp
+	VistaClusterSlave.h
+	VistaDataTunnel.cpp
+	VistaDataTunnel.h
+	VistaDataTunnelFactory.cpp
+	VistaDataTunnelFactory.h
+	VistaDriverPropertyConfigurator.cpp
+	VistaDriverPropertyConfigurator.h
+	VistaErrorMsgHandler.h
+	VistaKernelConfig.h
+	VistaKernelMsgPort.cpp
+	VistaKernelMsgPort.h
+	VistaKernelMsgTab.h
+	VistaKernelOut.cpp
+	VistaKernelOut.h
+	VistaMasterDataTunnel.cpp
+	VistaMasterDataTunnel.h
+	VistaStandaloneDataTunnel.cpp
+	VistaStandaloneDataTunnel.h
+	VistaSystem.cpp
+	VistaSystem.h
+	VistaSystemClassFactory.h
+	VistaSystemCommands.cpp
+	VistaSystemCommands.h
+	VistaSystemConfigurators.cpp
+	VistaSystemConfigurators.h
+)
+
+set( LocalSourceGroupFiles  )
+foreach( File ${DirFiles} )
+	list( APPEND LocalSourceGroupFiles "${RelativeDir}/${File}" )
+	list( APPEND ProjectSources "${RelativeDir}/${File}" )
+endforeach()
+source_group( "${LocalSourceGroup}" FILES ${LocalSourceGroupFiles} )
+
+set( SubDirFiles "" )
+foreach( Dir ${SubDirs} )
+	list( APPEND SubDirFiles "${RelativeDir}/${Dir}/_SourceFiles.cmake" )
+endforeach()
+
+foreach( SubDirFile ${SubDirFiles} )
+	include( ${SubDirFile} )
+endforeach()
+
