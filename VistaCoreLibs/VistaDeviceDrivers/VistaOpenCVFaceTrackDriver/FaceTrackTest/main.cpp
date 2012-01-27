16edef90208dbacf38193713ed430e1ea2125221
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2012-01-27 09:03:14 +0000

    UPD: FaceTrackTest
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/Vista/trunk@28990 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- VistaCoreLibs/VistaDeviceDrivers/VistaOpenCVFaceTrackDriver/FaceTrackTest/main.cpp
+++ VistaCoreLibs/VistaDeviceDrivers/VistaOpenCVFaceTrackDriver/FaceTrackTest/main.cpp
@@ -27,7 +27,7 @@
 #include <VistaKernel/VistaSystem.h>
 #include <VistaKernel/GraphicsManager/VistaGeometryFactory.h>
 #include <VistaKernel/GraphicsManager/VistaGraphicsManager.h>
-#include <VistaKernel/GraphicsManager/VistaSG.h>
+#include <VistaKernel/GraphicsManager/VistaSceneGraph.h>
 #include <VistaKernel/GraphicsManager/VistaGeomNode.h>
 #include <VistaKernel/GraphicsManager/VistaGroupNode.h>
 #include <VistaKernel/GraphicsManager/VistaTransformNode.h>
@@ -55,7 +55,7 @@ int	main( int argc, char* argv[] )
 	if( oSystem.Init( argc, argv ) == false )
 		return -1;
 
-	VistaSG* pSceneGraph = oSystem.GetGraphicsManager()->GetSceneGraph();
+	VistaSceneGraph* pSceneGraph = oSystem.GetGraphicsManager()->GetSceneGraph();
 	VistaGeometryFactory oFactory( pSceneGraph );
 	VistaGeometry* pGeom = oFactory.CreateBox( S_fCubeSize, S_fCubeSize, S_fCubeSize, 
 													1, 1, 1, VistaColorRGB::LIGHT_GRAY );
