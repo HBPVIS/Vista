f5245b013743f0258071612bc1c48ed774a2ffcb
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2012-02-10 18:09:25 +0000

    UPD: VistaDemo Cave startcripts+config
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/Vista/trunk@29206 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- VistaDemo/slave_cave.sh
+++ VistaDemo/slave_cave.sh
@@ -8,27 +8,33 @@ if [ "$1" = "" ]; then
 	echo "No demo executable specified!"
 	exit -4	
 else
-	EXECUTABLE="$1/$1"
-	PATHSCRIPT="$1/set_path_for_$1.sh"
+	EXECUTABLE="./$1"
+	PATHSCRIPT="set_path_for_$1.sh"
 fi
 
-if [ ! -x $EXECUTABLE ];
+if [ ! -d $1 ];
+then
+	echo "Specified dir $1 does not exist"
+	exit -5
+fi
+
+cd $1
+
+if [ ! -x $1 ];
 then
 	echo "Specified file $EXECUTABLE does not exist, or is not executable"
 	exit -5
-di
-	
 fi
-
+	
 if [ "$2" = "" ]; then
-	echo "No slave configuration given."
+	echo "No cluster configuration given."
 	exit -4	
 fi
 
 if [ -x $PATHSCRIPT ]; then
 	source $PATHSCRIPT
 fi
-$EXECUTABLE -clusterslave $2 -vistaini vista_cave.ini -interactionini interaction_cave -displayini display_cave.ini -clusterini display_cave.ini
+$EXECUTABLE  -clusterslave $2 -vistaini vista_cave.ini -interactionini interaction_cave.ini -displayini display_cave.ini -clusterini display_cave.ini
 
 
 
